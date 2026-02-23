00ca196592629dfa5b072200bff20a3ec272db47
404brtk <bzielonka1@proton.me>
2026-02-23 21:26:36 +0100

    feat: add initial configuration settings

---
+++ src/config.py
@@ -0,0 +1,13 @@
+from pydantic_settings import BaseSettings
+
+
+class Settings(BaseSettings):
+    model_config = {"env_prefix": "CRAWLER_", "env_file": ".env"}
+
+    HOST = "127.0.0.1"
+    PORT = 8001
+    LOG_LEVEL = "INFO"
+
+
+
+settings = Settings()
