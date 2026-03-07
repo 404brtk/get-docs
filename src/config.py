fc2ce60cca4a667603a00798803696f95a554c84
404brtk <bzielonka1@proton.me>
2026-03-07 17:39:06 +0100

    chore: add 'extra' option to model_config in Settings

--- src/config.py
+++ src/config.py
@@ -2,7 +2,7 @@ from pydantic_settings import BaseSettings
 
 
 class Settings(BaseSettings):
-    model_config = {"env_prefix": "GETDOCS_", "env_file": ".env"}
+    model_config = {"env_prefix": "GETDOCS_", "env_file": ".env", "extra": "ignore"}
 
     HOST: str = "127.0.0.1"
     PORT: int = 8001
