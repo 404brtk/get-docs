48e5a8df5a8a61e7e4088f829d1a502ec8c57743
404brtk <bzielonka1@proton.me>
2026-03-16 17:33:33 +0100

    refactor: remove datefmt from logger formatter so that milliseconds are included

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -10,7 +10,6 @@ def setup_logger(name: str = "get-docs") -> logging.Logger:
         handler = logging.StreamHandler(sys.stdout)
         formatter = logging.Formatter(
             "%(asctime)s | %(levelname)-8s | %(name)s | %(message)s",
-            datefmt="%Y-%m-%d %H:%M:%S",
         )
         handler.setFormatter(formatter)
         logger.addHandler(handler)
